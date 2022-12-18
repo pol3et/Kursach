99037cb129deb69d838bcb18927ea5ce7be0954b
Nikita Mazein <namer4666@gmail.com>
2022-12-19 02:15:21 +0300

    REG BACK IS READY

--- login/js/reg.js
+++ login/js/reg.js
@@ -1,58 +1,39 @@
-function postFetchForSignUp() {
-    let nameInput = document.getElementsByClassName("namee")
-    let passwordInput = document.getElementsByClassName("pas")
-    let passwordInput2 = document.getElementsByClassName("pas2")
-    console.log(nameInput, passwordInput, passwordInput2)
-    fetch('http://localhost:3000/users', { // First, we make a Post fetch request where we want to store our users.
-      method: 'POST',
-      headers: {
-        'Content-Type':'application/json',
-        'Accept':'application/json'
-      },
-      body: JSON.stringify({
-        name: nameInput.value,
-        password: passwordInput.value
-      })
-    })
-    .then(res=>res.json())
-    .then(user => {
-      localStorage.clear() // If there was a user signed in, this will clear it up                               
-      localStorage.id = user.id  // Then we can store the id we got 
-      slapUser(user)
-      logOutButton()
-    })
+const signUp = e => {
+  let name = document.getElementById('name').value,
+      login = document.getElementById('login').value,
+      password = document.getElementById('password').value;
+
+  let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+  let exist = formData.length && 
+      JSON.parse(localStorage.getItem('formData')).some(data => 
+          data.name.toLowerCase() == name.toLowerCase() && 
+          data.login.toLowerCase() == login.toLowerCase()
+      );
+
+  if(!exist){
+      formData.push({ name, login, password });
+      localStorage.setItem('formData', JSON.stringify(formData));
+      document.querySelector('form').reset();
+      document.getElementById('name').focus();
+      alert("Ура, твой аккаунт зарегестрирован!\n\nВернись на форму авторизации и войди в свой аккаунт.");
   }
+  else{
+      alert("Ууууууупс.....\nКажется такой аккаунт уже существует!\n\nПопробуй войти в свой аккаунт или зарегестрируй новый.");
+  }
+  e.preventDefault();
+}
 
-signInButton.addEventListener('click', e => {
-  signInForm()
-  let form = signDiv.querySelector('.sign-in')
-  let passwordInput = document.querySelector(".pas")
-  form.addEventListener('submit', e=>{
-    e.preventDefault()
-    fetch('http://localhost:3000/users') // We make a get fetch request where users are stored
-    .then(res=>res.json()) 
-    .then(usersArray => { 
-      let user = usersArray.find(function(user){ 
-          return user.password === passwordInput.value // Then we        // check if there is a user with a value given
-        })
-      if (user){
-        signDiv.innerHTML = ""
-        slapUser(user)
-        localStorage.id = user.id // If there is so, we then store // it
-        logOutButton()
-        writeReview()
-      }
-    })
-  })
-})
-
-function logOutButton(){
-  let logOutButton = document.createElement("button")
-  logOutButton.className = "log-out-button"
-  logOutButton.innerText = "Log Out"
-  signDiv.append(logOutButton)
-  logOutButton.addEventListener('click', e=>{
-    localStorage.clear() // We clear localStorage like so
-  })
- }
-postFetchForSignUp()
+function signIn(e) {
+  let login = document.getElementById('login').value, password = document.getElementById('password').value;
+  let formData = JSON.parse(localStorage.getItem('formData')) || [];
+  let exist = formData.length && 
+  JSON.parse(localStorage.getItem('formData')).some(data => data.login.toLowerCase() == login && data.password.toLowerCase() == password);
+  if(!exist){
+      alert("Барсик не знает такого человека :(\n\nПопробуй еще раз или зарегестрируй новый аккаунт.");
+  }
+  else{
+      location.href = "index2.html";
+  }
+  e.preventDefault();
+}
