a8db5100ebf9240d56f80e95706ee0b74bdbbd4a
Nikita Mazein <namer4666@gmail.com>
2022-12-19 13:58:09 +0300

    pops arrange

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,60 +7,9 @@ window.addEventListener("load", function(){
     )
 });
 
-window.addEventListener("load", function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".popup4").style.display = "block";
-        },
-        10
-    )
-});
-
-document.querySelector("#close4").addEventListener("click", function(){
-    document.querySelector(".popup4").style.display = "none"; 
-    setTimeout("showIt()", 20000);
-});
 document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 });
 document.querySelector("#close2").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
-<<<<<<< HEAD
 });
-
-function showIt() {
-    document.getElementById("popup3").style.visibility = "visible";
-  }
- // after 60 seconds
-
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup2").style.display = "none";
-});
-document.querySelector("#close2").addEventListener("click", function(){
-    document.querySelector(".popup2").style.display = "none";
-});
-
-const showStats = e => {
-    var cntr = localStorage.getItem('eqc');
-    console.log(cntr);
-    var seqcounter = cntr - 1;
-    var mscore = seqcounter*10;
-    var previousScrore = localStorage.getItem('score');
-    var currentScore = previousScrore + mscore;
-    localStorage.setItem('score', currentScore);
-    document.getElementById('s-equation-counter').innerHTML = seqcounter;
-    document.getElementById('m-score').innerHTML = mscore; 
-  }
-var cntr = localStorage.getItem('eqc');
-console.log(cntr);
-var seqcounter = cntr - 1;
-var mscore = seqcounter*10;
-var previousScrore = localStorage.getItem('score');
-var currentScore = previousScrore + mscore;
-localStorage.setItem('score', currentScore);
-document.getElementById('s-equation-counter').innerHTML = seqcounter;
-document.getElementById('m-score').innerHTML = mscore;
-
-=======
-});
->>>>>>> 8e851b960f854bdcd46100d3f38c7e052292979e
